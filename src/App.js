cc987d7f7fdca6ff679a6c21d77f915ea1b5f3cf
Zentozzz <funikov159@gmail.com>
2022-06-15 18:57:13 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+  const [textInputPush, setTextInputPush] = React.useState('');
+  const [inputPush, setInputPush] = React.useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="window">
+      <input
+        type="text"
+        placeholder="Введите задачу"
+        value={textInputPush}
+        onChange={(e) => setTextInputPush(e.target.value)}
+      />
+      <div className="tasks">{inputPush}</div>
     </div>
   );
 }
